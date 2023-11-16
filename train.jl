ef67138bec3f90b3e9da5e2fed8ab676b6d001f0
Tzu-Hsuan Chen <114680301+THChen0124@users.noreply.github.com>
2023-11-16 12:52:04 -0500

    Update train.jl

--- train.jl
+++ train.jl
@@ -4,15 +4,15 @@ DESCRIPTION
 This loop initiates a parallel computing process. 
 """
 
-# Directory for saving output files (replace this w/ your directory)
-data_dir = "..."
+# Directory for saving output files 
+data_dir = "..."  # Replace "..." with the directory where you want to save output files
 
 @sync @distributed for i = 0:7
     # Call 'train_formation_energy' function
     model, results = train_formation_energy(num_pts = 20375, IE=I, EA=EA, VO=V, GR=G, RO=P ,BL=B ,CR=R ,X=X, VA=VA)
     
     # Construct the filepath for the output file, unique for each worker process
-    filepath = string(data_dir, i, ".txt")
+    filepath = string(data_dir, i, ".txt")  # Modify this line to assign the desired file name to each worker
 
     # Write the results of the training process to a text file
     open(filepath, "w") do f
